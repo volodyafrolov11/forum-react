76c444e993b1263c7a82ad126c0f0cbf09f565f5
volodyafrolov11 <volodyavolodya666@yandex.ru>
2023-09-08 15:09:52 +0300

    add dropdown, update navbar, add avatar with menu

---
+++ src/shared/ui/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { Button } from '../Button/Button';
+import { Dropdown } from './Dropdown';
+
+export default {
+    title: 'shared/Dropdown',
+    component: Dropdown,
+    decorators: [
+        (Story) => <div style={{ padding: 100 }}><Story /></div>,
+    ],
+} as ComponentMeta<typeof Dropdown>;
+
+const Template: ComponentStory<typeof Dropdown> = (args) => <Dropdown {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    trigger: <Button>Open</Button>,
+    items: [
+        {
+            content: 'first',
+        },
+        {
+            content: 'second',
+        },
+        {
+            content: 'third',
+        },
+    ],
+};
+
+export const topRight = Template.bind({});
+topRight.args = {
+    direction: 'top right',
+    trigger: <Button>Open</Button>,
+    items: [
+        {
+            content: 'first',
+        },
+        {
+            content: 'second',
+        },
+        {
+            content: 'third',
+        },
+    ],
+};
+
+export const topLeft = Template.bind({});
+topLeft.args = {
+    direction: 'top left',
+    trigger: <Button>Open</Button>,
+    items: [
+        {
+            content: 'first',
+        },
+        {
+            content: 'second',
+        },
+        {
+            content: 'third',
+        },
+    ],
+};
+
+export const bottomRight = Template.bind({});
+bottomRight.args = {
+    direction: 'bottom right',
+    trigger: <Button>Open</Button>,
+    items: [
+        {
+            content: 'first',
+        },
+        {
+            content: 'second',
+        },
+        {
+            content: 'third',
+        },
+    ],
+};
+
+export const bottomLeft = Template.bind({});
+bottomLeft.args = {
+    direction: 'bottom left',
+    trigger: <Button>Open</Button>,
+    items: [
+        {
+            content: 'first',
+        },
+        {
+            content: 'second',
+        },
+        {
+            content: 'third',
+        },
+    ],
+};
--- src/shared/ui/ListBox/ListBox.stories.tsx
+++ src/shared/ui/ListBox/ListBox.stories.tsx
@@ -1,14 +1,63 @@
 import React from 'react';
-import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { ListBox } from './ListBox';
 
 export default {
     title: 'shared/ListBox',
     component: ListBox,
+    decorators: [
+        (Story) => <div style={{ padding: 100 }}><Story /></div>,
+    ],
 } as ComponentMeta<typeof ListBox>;
 
-const Template: ComponentStory<typeof ListBox> = (args) => <ListBox />;
+const Template: ComponentStory<typeof ListBox> = (args) => <ListBox {...args} />;
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+    value: '123',
+    items: [
+        { content: '1asfasfasf23', value: '123' },
+        { content: '1asfasfasf21233', value: '1232' },
+    ],
+};
+
+export const topLeft = Template.bind({});
+topLeft.args = {
+    direction: 'top left',
+    value: '123',
+    items: [
+        { content: '1asfasfasf23', value: '123' },
+        { content: '1asfasfasf21233', value: '1232' },
+    ],
+};
+
+export const topRight = Template.bind({});
+topRight.args = {
+    direction: 'top right',
+    value: '123',
+    items: [
+        { content: '1asfasfasf23', value: '123' },
+        { content: '1asfasfasf21233', value: '1232' },
+    ],
+};
+
+export const bottomLeft = Template.bind({});
+bottomLeft.args = {
+    direction: 'bottom left',
+    value: '123',
+    items: [
+        { content: '1asfasfasf23', value: '123' },
+        { content: '1asfasfasf21233', value: '1232' },
+    ],
+};
+
+export const bottomRight = Template.bind({});
+bottomRight.args = {
+    direction: 'bottom right',
+    value: '123',
+    items: [
+        { content: '1asfasfasf23', value: '123' },
+        { content: '1asfasfasf21233', value: '1232' },
+    ],
+};
