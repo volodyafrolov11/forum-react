61e20d52b45a67d31d806c833e55f1ddd9fcc084
volodyafrolov11 <volodyavolodya666@yandex.ru>
2023-09-24 14:18:29 +0300

    add theme addon for storybook

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -14,6 +14,14 @@ export const parameters = {
         },
     },
     layout: 'fullscreen',
+    themes: {
+        default: 'light',
+        list: [
+            { name: 'light', class: Theme.LIGHT, color: '#ffffff' },
+            { name: 'dark', class: Theme.DARK, color: '#011079' },
+            { name: 'orange', class: Theme.ORANGE, color: '#f57d04' },
+        ],
+    },
 };
 
 addDecorator(StyleDecorator);
