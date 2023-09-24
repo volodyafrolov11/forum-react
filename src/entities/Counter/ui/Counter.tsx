a637eca7bb0b0ed45f729fb09008728d772909d0
volodyafrolov11 <volodyavolodya666@yandex.ru>
2023-09-24 16:35:55 +0300

    add helpers for slices, selectors, actions

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,31 +1,30 @@
-import { useDispatch, useSelector } from 'react-redux';
 import { Button } from '@/shared/ui/Button';
-import { counterActions } from '../model/slice/counterSlice';
-import { getCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
+import { useCounterActions } from '../model/slice/counterSlice';
+import { useCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
 
 export const Counter = () => {
-    const dispatch = useDispatch();
-    const counterValue = useSelector(getCounterValue);
+    const counterValue = useCounterValue();
+    const { decrement, increment } = useCounterActions();
 
-    const increment = () => {
-        dispatch(counterActions.increment());
+    const handleIncrement = () => {
+        increment();
     };
 
-    const decrement = () => {
-        dispatch(counterActions.decrement());
+    const handleDecrement = () => {
+        decrement();
     };
 
     return (
         <div>
             <h1 data-testid="value-title">{counterValue}</h1>
             <Button
-                onClick={increment}
+                onClick={handleIncrement}
                 data-testid="increment-btn"
             >
                 increment
             </Button>
             <Button
-                onClick={decrement}
+                onClick={handleDecrement}
                 data-testid="decrement-btn"
             >
                 decrement
