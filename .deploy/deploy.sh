5c548c704ed5108d520616f3c41ff55af615fa25
volodyafrolov11 <volodyavolodya666@yandex.ru>
2023-09-30 13:02:12 +0300

    add args for selector

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -2,4 +2,4 @@ cd ~/forum-react
 npm run build:prod
 
 rm -rf ~/../var/www/forum-react/html
-mv ~/forum-react/build/ ~/../var/www/forum-react/html
+mv ~/forum-react/build ~/../var/www/forum-react/html/
